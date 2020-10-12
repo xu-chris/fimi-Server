abebdef6fbb64c7b51443e37dec9b8209b0d1ac7
Chris Reichel <chrisreichel@pm.me>
2020-10-12 17:41:34 +0200

    Add message handler

--- src/periphery/WebsocketServer.h
+++ src/periphery/WebsocketServer.h
@@ -1,5 +1,5 @@
-#ifndef _WEBSOCKET_SERVER
-#define _WEBSOCKET_SERVER
+#ifndef WEBSOCKETSERVER_H
+#define WEBSOCKETSERVER_H
 
 //We need to define this when using the Asio library without Boost
 #define ASIO_STANDALONE
@@ -12,6 +12,7 @@
 #include <vector>
 #include <mutex>
 #include <map>
+
 using std::string;
 using std::vector;
 using std::map;
@@ -19,56 +20,56 @@ using std::map;
 typedef websocketpp::server<websocketpp::config::asio> WebsocketEndpoint;
 typedef websocketpp::connection_hdl ClientConnection;
 
-class WebsocketServer
-{
-	public:
-		
-		WebsocketServer();
-		void run(int port);
-		
-		//Returns the number of currently connected clients
-		size_t numConnections();
-		
-		//Registers a callback for when a client connects
-		template <typename CallbackTy>
-		void connect(CallbackTy handler)
-		{
-			//Make sure we only access the handlers list from the networking thread
-			this->eventLoop.post([this, handler]() {
-				this->connectHandlers.push_back(handler);
-			});
-		}
-		
-		//Registers a callback for when a client disconnects
-		template <typename CallbackTy>
-		void disconnect(CallbackTy handler)
-		{
-			//Make sure we only access the handlers list from the networking thread
-			this->eventLoop.post([this, handler]() {
-				this->disconnectHandlers.push_back(handler);
-			});
-		}
-		
-		//Sends a message to an individual client
-		//(Note: the data transmission will take place on the thread that called WebsocketServer::run())
-        void sendMessage(ClientConnection conn, const string &message);
-		
-		//Sends a message to all connected clients
-		//(Note: the data transmission will take place on the thread that called WebsocketServer::run())
-        void broadcastMessage(const string &message);
-		
-	protected:
-		
-		void onOpen(const ClientConnection& conn);
-		void onClose(const ClientConnection& conn);
-		
-		asio::io_service eventLoop;
-		WebsocketEndpoint endpoint;
-		vector<ClientConnection> openConnections;
-		std::mutex connectionListMutex;
-		
-		vector<std::function<void(ClientConnection)>> connectHandlers;
-		vector<std::function<void(ClientConnection)>> disconnectHandlers;
+class WebsocketServer {
+public:
+    std::function<void(std::string)> messageHandler;
+
+    WebsocketServer();
+
+    void run(int port);
+
+    //Returns the number of currently connected clients
+    size_t numConnections();
+
+    //Registers a callback for when a client connects
+    template<typename CallbackTy>
+    void connect(CallbackTy handler) {
+        //Make sure we only access the handlers list from the networking thread
+        this->eventLoop.post([this, handler]() {
+            this->connectHandlers.push_back(handler);
+        });
+    }
+
+    //Registers a callback for when a client disconnects
+    template<typename CallbackTy>
+    void disconnect(CallbackTy handler) {
+        //Make sure we only access the handlers list from the networking thread
+        this->eventLoop.post([this, handler]() {
+            this->disconnectHandlers.push_back(handler);
+        });
+    }
+
+    //Sends a message to an individual client
+    //(Note: the data transmission will take place on the thread that called WebsocketServer::run())
+    void sendMessage(ClientConnection conn, const string &message);
+
+    //Sends a message to all connected clients
+    //(Note: the data transmission will take place on the thread that called WebsocketServer::run())
+    void broadcastMessage(const string &message);
+
+protected:
+
+    void onOpen(const ClientConnection &conn);
+    void onClose(const ClientConnection &conn);
+    void onMessage(const websocketpp::connection_hdl& hdl, const WebsocketEndpoint::message_ptr& msg);
+
+    asio::io_service eventLoop;
+    WebsocketEndpoint endpoint;
+    vector<ClientConnection> openConnections;
+    std::mutex connectionListMutex;
+
+    vector<std::function<void(ClientConnection)>> connectHandlers;
+    vector<std::function<void(ClientConnection)>> disconnectHandlers;
 };
 
 #endif
